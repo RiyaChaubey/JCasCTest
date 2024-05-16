ffc693d9c00992721fe4efb7c060cf5f9d458cf7
RiyaChaubey <riya.chaubey@ericom.com>
2024-05-16 16:07:14 +0530

    PEZTE-563 print issue

--- move_cases_to_ci_verified.py
+++ move_cases_to_ci_verified.py
@@ -77,6 +77,7 @@ def update_fixed_in_build(issue, fixed_in_build):
 for x in data['cases']:
     try:
         issue=jira.issue(x)
+        print(issue)
         if x.startswith("SHIELD-") or x.startswith("AC-"):
             print("Moving case:{} to CI-Verified".format(x))
             moveCastState(x,"CI-Verified")
